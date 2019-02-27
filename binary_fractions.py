7ef73ec89a0aeaa0cb5cad1be124340f58dcc4f8
chipfranzen <chip.franzen@gmail.com>
2019-02-26 22:03:59 -0800

    Formatting.

--- binary_fractions.py
+++ binary_fractions.py
@@ -1,7 +1,7 @@
-'''
+"""
 Utils for working with binary fractions.
 Binary fractions are represented as strings of the form "0b.000"
-'''
+"""
 
 import re
 
@@ -10,11 +10,13 @@ import numpy as np
 
 def bin_to_dec(bin_string):
     pattern = r"0b.[01]*"
-    assert re.fullmatch(pattern, bin_string), f"Binary string {bin_string} does not match {pattern}."
+    assert re.fullmatch(
+        pattern, bin_string
+    ), f"Binary string {bin_string} does not match {pattern}."
 
     bin_string = bin_string[3:]
 
-    dec = 0.
+    dec = 0.0
 
     for i, e in enumerate(bin_string):
         dec += int(e) * 1 / 2 ** (i + 1)
@@ -23,20 +25,20 @@ def bin_to_dec(bin_string):
 
 
 def dec_to_bin(dec):
-    assert 0. < dec < 1.
+    assert 0.0 < dec < 1.0
 
     bin_string = "0b."
 
     i = 1
-    while not (np.isclose(dec, 0.)):
+    while not (np.isclose(dec, 0.0)):
 
-        power = 1/2**i
+        power = 1 / 2 ** i
         if dec >= power:
-            next_char = '1'
+            next_char = "1"
             dec -= power
 
         else:
-            next_char = '0'
+            next_char = "0"
 
         bin_string += next_char
         i += 1
@@ -46,7 +48,8 @@ def dec_to_bin(dec):
 
 def main():
     bin_to_dec("0b.10")
-    dec_to_bin(1/2 + 1/64 + 1/1024)
+    dec_to_bin(1 / 2 + 1 / 64 + 1 / 1024)
+
 
 if __name__ == "__main__":
     main()
--- test_.py
+++ test_.py
@@ -1,12 +1,13 @@
 from binary_fractions import bin_to_dec, dec_to_bin
 
+
 def test_bin_to_dec():
 
     test_string = "0b.011"
-    assert 1/4 + 1/8 == bin_to_dec(test_string)
+    assert 1 / 4 + 1 / 8 == bin_to_dec(test_string)
+
 
 def test_dec_to_bin():
 
-    test_float = 1/2 + 1/64 + 1/1024
-    assert '0b.1000010001' == dec_to_bin(test_float)
-
+    test_float = 1 / 2 + 1 / 64 + 1 / 1024
+    assert "0b.1000010001" == dec_to_bin(test_float)
